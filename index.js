f751ab940257300658bfd87fd2e29c7c71b109da
Felipe Fantoni <felipe.fantoni@hotmail.com>
2020-02-29 01:33:13 -0300

    Conversao CSV

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
 const axios = require("axios");
 const fs = require("fs");
+const Json2Csv = require("json2csv").parse;
+var after = "";
+var resposta = [];
+var pagina = 0;
+var atual = 0;
+const limite = 20;
+var csv;
 
 axios.defaults.headers.common["Authorization"] =
-  "Bearer 9bb7b70e88eb66e2030a9fa0b2d508b8a8c571e9";
+  "Bearer 740fe9617dbd29e70f4037d48f61ea7d5eb97d37";
 
 const getResultado = async () => {
   try {
     const query = `query RepositoriosPopulares {
-            search(query: "stars:>100", type: REPOSITORY, first: 100) {
+            search(query: "stars:>100", type: REPOSITORY, first:20${after}) {
+              pageInfo {
+                hasNextPage
+                endCursor
+              }
               nodes {
                 ... on Repository {
                   nameWithOwner
                   createdAt
-                  pullRequests {
+                  pullRequests (states: MERGED){
                     totalCount
                   }
                   releases {
@@ -39,12 +50,22 @@ const getResultado = async () => {
       query: query
     });
     const resultado = consulta.data.data.search;
-    fs.writeFile("resultadoConsulta.json", JSON.stringify(resultado), err =>
-      console.log(err)
-    );
-    console.log("Arquivo criado");
+    if (resultado.pageInfo.hasNextPage && atual <= limite) {
+      after = `, after: "${resultado.pageInfo.endCursor}"`;
+      resposta = resposta.concat(resultado.nodes);
+      pagina += 20;
+      atual += 20;
+      console.log("Página número: " + pagina);
+      await getResultado();
+    } else {
+      //fs.writeFile("resultadoConsulta.json", JSON.stringify(resposta), err => console.log(err));
+      csv = Json2Csv(resposta, {fields: ["nameWithOwner", "createdAt", "pullRequests", "releases", "updatedAt", "primaryLanguage", "closedIssues", "totalIssues", "stargazers"]});
+      fs.writeFileSync("resultadoFinal.csv", csv);
+      console.log("Arquivo criado");
+    }
   } catch (e) {
     console.log(e);
+    await getResultado();
   }
 };
 
